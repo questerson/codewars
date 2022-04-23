d1a2f77416a02e677bfabbd74283535c8ef0d449
questerson <yeadoneric@gmail.com>
2022-04-22 22:43:52 -0400

    adding String Cleaning

---
+++ js/string_cleaning.js
@@ -0,0 +1,31 @@
+function stringClean(s){
+    return s.split(/\d/).join("");
+  }
+  
+  
+  
+  
+  function stringClean(s){
+    let result = '';
+    for(let i = 0; i < s.length; i++){
+      if(!isNaN(Number(s[i])) && s[i] !== ' ') continue;
+      result += s[i];
+    }
+    return result;
+  }
+  
+  
+  
+  
+  
+  function stringClean(s){
+    const nums = "1234567890"
+    let cleanStr = ""
+  
+    for (let i = 0; i < s.length; i++) {
+      if (nums.includes(s[i])) continue
+      else cleanStr += s[i]
+    }
+    
+    return cleanStr
+  }
