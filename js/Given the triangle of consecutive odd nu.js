c2c869567862594c53d842bccf52609403d631b9
questerson <yeadoneric@gmail.com>
2022-09-07 20:05:51 -0400

    find the sum of nth row triangle codewars

---
+++ js/Given the triangle of consecutive odd nu.js
@@ -0,0 +1,18 @@
+Given the triangle of consecutive odd numbers:
+
+             1           1
+          3     5           2^2
+       7     9    11          3^4
+   13    15    17    19         4^4
+21    23    25    27    29      5^5
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
+
+function rowSumOddNumbers(n) {
+	// TODO
+  
+  return n**3
+}
